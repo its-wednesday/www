4db54ae834a5bcfb2a52f5390155010c2f0a4933
nigelwtf <nigel@sirisomphone.com>
2019-05-02 09:44:23 +1200

    Fix logo component. ECR/ECS circle config.

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -2,19 +2,18 @@ import styled from 'styled-components';
 import { spacing } from 'styles/spacing';
 
 const LogoAnchor = styled.a`
-  display: block;
+  display: inline-block;
   margin-bottom: ${spacing(3)}px;
   /* override the hover underline */
   border: none !important;
 `;
 
 const LogoImage = styled.img`
-  display: block;
   height: 128px;
 `;
 
 export const Logo = () => (
   <LogoAnchor href="/">
-    <LogoImage src="/static/images/logo.svg" alt="It's Wednesday | Digital application consultancy" />
+    <LogoImage src="/static/images/logo.svg" alt="It's Wednesday" />
   </LogoAnchor>
 );
